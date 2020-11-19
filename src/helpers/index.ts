9ed028ebfc4802fcaeaf69fe056154f0f568bea1
jayree <jayree.git@icloud.com>
2020-11-19 23:17:20 +0100

    OAuth 1.0a support

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from 'axios';
 import * as jwt from 'atlassian-jwt';
 import * as url from 'url';
+import Oauth1a from 'oauth-1.0a';
+import * as crypto from 'crypto';
 import { Config } from '../config';
 
 export const getAuthentication = (
@@ -28,6 +30,29 @@ export const getAuthentication = (
     );
 
     return `JWT ${jwtToken}`;
+  }
+
+  if (config.authentication?.oauth1) {
+    const oauth = new Oauth1a({
+      consumer: {
+        key: config.authentication.oauth1.consumerKey,
+        secret: config.authentication.oauth1.consumerSecret,
+      },
+      signature_method: 'RSA-SHA1',
+      hash_function(base_string: string, key: string) {
+        return crypto.createSign('RSA-SHA1').update(base_string).sign(decodeURIComponent(key), 'base64');
+      },
+    });
+
+    const authorization = oauth.authorize(
+      { url: config.host + request.url!, method: request.method! },
+      {
+        key: config.authentication.oauth1.tokenKey,
+        secret: config.authentication.oauth1.tokenSecret,
+      },
+    );
+
+    return oauth.toHeader(authorization).Authorization;
   }
 
   if (config.authentication?.accessToken) {
